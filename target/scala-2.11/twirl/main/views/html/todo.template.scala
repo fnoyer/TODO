140107a2ea7ade11ac538190b423f56d02ef17f5
DESKTOP-BSDRKTP\Frederic Noyer <fredericnoyer.nyc@gmail.com>
2017-01-21 22:52:54 +0100

    Last change in view with header and date printed elegently in footer

--- target/scala-2.11/twirl/main/views/html/todo.template.scala
+++ target/scala-2.11/twirl/main/views/html/todo.template.scala
@@ -31,40 +31,41 @@ Seq[Any](format.raw/*6.43*/("""
 """),format.raw/*13.3*/("""
 """),format.raw/*15.1*/("""
 """),_display_(/*16.2*/main("TODO LIST")/*16.19*/ {_display_(Seq[Any](format.raw/*16.21*/("""
-
-    """),format.raw/*18.5*/("""<div class="container">
-        <h1>"""),_display_(/*19.14*/(new Date().toString)),format.raw/*19.35*/("""</h1>
+    """),format.raw/*17.5*/("""<div class="container">
+        <div class="page-header" align="center">
+            <h1>Life is good!</h1>
+        </div>
         <div class="panel-group">
             <div class="panel panel-sample">
-                <div class="panel-heading" align="center"><h3>There is <span class="badge">"""),_display_(/*22.93*/tasks/*22.98*/.size),format.raw/*22.103*/("""</span> task"""),_display_(/*22.116*/if(tasks.size > 1)/*22.134*/{_display_(Seq[Any](format.raw/*22.135*/("""s""")))}),format.raw/*22.137*/(""" """),format.raw/*22.138*/("""on the pipe!</h3></div>
+                <div class="panel-heading" align="center"><h3>There is <span class="badge">"""),_display_(/*23.93*/tasks/*23.98*/.size),format.raw/*23.103*/("""</span> active task"""),_display_(/*23.123*/if(tasks.size > 1)/*23.141*/{_display_(Seq[Any](format.raw/*23.142*/("""s""")))}),format.raw/*23.144*/("""</h3></div>
                 <div class="panel-body">
-                    """),_display_(/*24.22*/for(task <- tasks) yield /*24.40*/{_display_(Seq[Any](format.raw/*24.41*/("""
-                        """),format.raw/*25.25*/("""<div class="row">
-                                """),_display_(/*26.34*/helper/*26.40*/.form(routes.HomeController.deleteTodo(task.id.getOrElse(0)))/*26.101*/ {_display_(Seq[Any](format.raw/*26.103*/("""
-                                    """),format.raw/*27.37*/("""<div class="col-sm-1"><button type="submit" class="btn btn-sample btn-sm" >&#10004; Done</button></div>
-                                    <div class="bg-sample col-sm-5" align="left" ><h5>"""),_display_(/*28.88*/task/*28.92*/.content),format.raw/*28.100*/("""</h5></div>
-                                """)))}),format.raw/*29.34*/("""
-                                """),_display_(/*30.34*/helper/*30.40*/.form(routes.HomeController.updateTodo(task.id.getOrElse(0)))/*30.101*/ {_display_(Seq[Any](format.raw/*30.103*/("""
-                                    """),format.raw/*31.37*/("""<div class="col-sm-5"><input class="form-control input-xm" placeholder="update" name="content" type="text"></div>
+                    """),_display_(/*25.22*/for(task <- tasks) yield /*25.40*/{_display_(Seq[Any](format.raw/*25.41*/("""
+                        """),format.raw/*26.25*/("""<div class="row">
+                                """),_display_(/*27.34*/helper/*27.40*/.form(routes.HomeController.deleteTodo(task.id.getOrElse(0)))/*27.101*/ {_display_(Seq[Any](format.raw/*27.103*/("""
+                                    """),format.raw/*28.37*/("""<div class="col-sm-1"><button type="submit" class="btn btn-sample btn-sm" >&#10004; Done</button></div>
+                                    <div class="bg-sample col-sm-5" align="left" ><h5>"""),_display_(/*29.88*/task/*29.92*/.content),format.raw/*29.100*/("""</h5></div>
+                                """)))}),format.raw/*30.34*/("""
+                                """),_display_(/*31.34*/helper/*31.40*/.form(routes.HomeController.updateTodo(task.id.getOrElse(0)))/*31.101*/ {_display_(Seq[Any](format.raw/*31.103*/("""
+                                    """),format.raw/*32.37*/("""<div class="col-sm-5"><input class="form-control input-xm" placeholder="update" name="content" type="text"></div>
                                     <div class="col-sm-1"><button type="submit" class="btn btn-sample btn-sm" >&#9998; Update</button></div>
-                                """)))}),format.raw/*33.34*/("""
-                        """),format.raw/*34.25*/("""</div>
+                                """)))}),format.raw/*34.34*/("""
+                        """),format.raw/*35.25*/("""</div>
                         <div class="row">
                             <div class="col-sm-8 col-md-offset-2"><hr></div>
                         </div>
-                    """)))}),format.raw/*38.22*/("""
-                    """),format.raw/*39.21*/("""<div class="row">
-                        """),_display_(/*40.26*/helper/*40.32*/.form(routes.HomeController.newTodo)/*40.68*/ {_display_(Seq[Any](format.raw/*40.70*/("""
-                            """),format.raw/*41.29*/("""<div class="col-sm-11"><input class="form-control input-sm" placeholder="content" name="content" type="text" value="" required></div>
+                    """)))}),format.raw/*39.22*/("""
+                    """),format.raw/*40.21*/("""<div class="row">
+                        """),_display_(/*41.26*/helper/*41.32*/.form(routes.HomeController.newTodo)/*41.68*/ {_display_(Seq[Any](format.raw/*41.70*/("""
+                            """),format.raw/*42.29*/("""<div class="col-sm-11"><input class="form-control input-sm" placeholder="content" name="content" type="text" value="" required></div>
                             <div class="col-sm-1">
                                 <button type="submit" class="btn btn-sample btn-sm btn-block  btn-sm"><span class="glyphicon glyphicon-plus"></span></button>
                             </div>
-                        """)))}),format.raw/*45.26*/("""
-                    """),format.raw/*46.21*/("""</div>
+                        """)))}),format.raw/*46.26*/("""
+                    """),format.raw/*47.21*/("""</div>
                 </div>
                 <div class="panel-footer" align="center">
-                    Todo List Webb App by Frédéric Noyer @ HEIG-VD 2017
-                </div>
+                """),_display_(/*50.18*/(new Date().toString)),format.raw/*50.39*/("""
+                """),format.raw/*51.17*/("""</div>
             </div>
         </div>
     </div>
@@ -91,11 +92,11 @@ Seq[Any](format.raw/*6.43*/("""
 object todo extends todo_Scope0.todo
               /*
                   -- GENERATED --
-                  DATE: Fri Jan 20 00:24:12 CET 2017
+                  DATE: Sat Jan 21 22:49:12 CET 2017
                   SOURCE: C:/Users/Frederic Noyer/CloudStation/HEIG-VD/3eme/POO/Todo/app/views/todo.scala.html
-                  HASH: 22609134db524c0e4247dff9330eb1876427540d
-                  MATRIX: 629->98|786->139|819->339|848->365|877->368|903->385|943->387|978->395|1043->433|1085->454|1292->634|1306->639|1333->644|1374->657|1402->675|1442->676|1476->678|1506->679|1621->767|1655->785|1694->786|1748->812|1827->864|1842->870|1913->931|1954->933|2020->971|2239->1163|2252->1167|2282->1175|2359->1221|2421->1256|2436->1262|2507->1323|2548->1325|2614->1363|2935->1653|2989->1679|3202->1861|3252->1883|3323->1927|3338->1933|3383->1969|3423->1971|3481->2001|3919->2408|3969->2430
-                  LINES: 23->6|28->6|31->13|32->15|33->16|33->16|33->16|35->18|36->19|36->19|39->22|39->22|39->22|39->22|39->22|39->22|39->22|39->22|41->24|41->24|41->24|42->25|43->26|43->26|43->26|43->26|44->27|45->28|45->28|45->28|46->29|47->30|47->30|47->30|47->30|48->31|50->33|51->34|55->38|56->39|57->40|57->40|57->40|57->40|58->41|62->45|63->46
+                  HASH: 9d567d1be625131668dcddb3b66ebc4d6b0b41e1
+                  MATRIX: 629->98|786->139|819->339|848->365|877->368|903->385|943->387|976->393|1303->693|1317->698|1344->703|1392->723|1420->741|1460->742|1494->744|1597->820|1631->838|1670->839|1724->865|1803->917|1818->923|1889->984|1930->986|1996->1024|2215->1216|2228->1220|2258->1228|2335->1274|2397->1309|2412->1315|2483->1376|2524->1378|2590->1416|2911->1706|2965->1732|3178->1914|3228->1936|3299->1980|3314->1986|3359->2022|3399->2024|3457->2054|3895->2461|3945->2483|4080->2591|4122->2612|4168->2630
+                  LINES: 23->6|28->6|31->13|32->15|33->16|33->16|33->16|34->17|40->23|40->23|40->23|40->23|40->23|40->23|40->23|42->25|42->25|42->25|43->26|44->27|44->27|44->27|44->27|45->28|46->29|46->29|46->29|47->30|48->31|48->31|48->31|48->31|49->32|51->34|52->35|56->39|57->40|58->41|58->41|58->41|58->41|59->42|63->46|64->47|67->50|67->50|68->51
                   -- GENERATED --
               */
           
